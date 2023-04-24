a7b8fc0afe0735b4462d2ae58c8dadd864cbfc63
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-04-24 11:43:28 +0300

    add all section

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -15,8 +15,8 @@ export class Searchbar extends Component {
           <input
             className={css.SearchForm_input}
             type="text"
-            autocomplete="off"
-            autofocus
+            autoComplete="off"
+            autoFocus
             placeholder="Search images and photos"
           />
         </form>
