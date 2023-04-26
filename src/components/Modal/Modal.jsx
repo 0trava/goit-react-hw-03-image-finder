8281b6bdd6385a67340c204867682462d2fd061e
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-04-26 23:55:00 +0300

    fix image

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,5 +1,6 @@
 // import * as basicLightbox from 'basiclightbox'
 import css from './Modal.module.css';
+import PropTypes from 'prop-types'; // типизація пропсів
 
 
 export const Modal = ({closeModal, src, alt})=>{
@@ -13,3 +14,8 @@ export const Modal = ({closeModal, src, alt})=>{
 }
 
 
+Modal.propTypes = {
+  closeModal: PropTypes.func.isRequired, // функція
+  src: PropTypes.string.isRequired, // строка
+  alt: PropTypes.string.isRequired // строка
+};
