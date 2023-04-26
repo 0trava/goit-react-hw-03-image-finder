5c50c16fd26be3f33cf84cd604b19efb1d0ab5c0
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-04-26 23:39:54 +0300

    modal console.log delete

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -3,7 +3,6 @@ import css from './Modal.module.css';
 
 
 export const Modal = ({closeModal, src, alt})=>{
-      console.log(src);
     return(
       <div className={css.Overlay} onClick={closeModal}>
         <div className={css.Modal}>
@@ -12,21 +11,5 @@ export const Modal = ({closeModal, src, alt})=>{
       </div>
     )
 }
-
-
-
-// Під час кліку на елемент галереї повинно відкриватися модальне вікно з темним 
-// оверлеєм і відображатися велика версія зображення. Модальне вікно повинно 
-// закриватися по натисканню клавіші ESC або по кліку на оверлеї.
-
-// Зовнішній вигляд схожий на функціонал цього VanillaJS-плагіна, 
-// тільки замість білого модального вікна рендериться зображення 
-// (у прикладі натисніть Run). Анімацію робити не потрібно!
-
-// const instance = basicLightbox.create(`
-//     <iframe src="https://www.youtube.com/embed/E1oZhEIrer4" width="560" height="315" frameborder="0"></iframe>
-// `)
-
-// instance.show()
 
 
