87f80ef8758b76803ebb4713b856d25be97b1809
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-04-26 22:45:17 +0300

    add load more btn

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,19 +7,20 @@ import {Modal} from './Modal/Modal';
 
 
 
-const API_CAY = '33995663-3283b38da6c47940fd5e67885';
-let PAGE = 1;
-let openList = '';
-let total = 1;
-// const URL = 'https://pixabay.com/api/?q=cat&page=1&key=your_key&image_type=photo&orientation=horizontal&per_page=12';
-
+const MY_API_KEY = '33995663-3283b38da6c47940fd5e67885'; // мій персональний ключ з pixabay
+const BASE_URL = 'https://pixabay.com/api/';
+let PHOTO_NAME ="";
+let showBtn = false;
 
 
 export class App extends Component {
 
   state = {
     imagelist: [],
-    total: 1,
+    photoName: "",
+    totalHits: 0,
+    page: 1,
+    stopPage: 1,
     filter: '',
     loading: false,
     showModal: false,
@@ -38,28 +39,56 @@ handleChange = (event) => {
 // FILTER - запуск команди пошуку
 searchBtnClick = (e) => {
   e.preventDefault(); // Зупиняємо оновлення сторінки
-  openList = this.state.filter;
-  console.log(openList);
-  fetch (`https://pixabay.com/api/?q=${this.state.filter}&page=1&key=${API_CAY}&image_type=photo&orientation=horizontal&per_page=12`)
-  .then(res => res.json())
-  .then (imagelist => this.setState({imagelist}));
-  total = this.state.imagelist.total;
-  console.log(total);
-  this.setState( { filter: ''}); // очищення вмісту форми
-}
+  PHOTO_NAME = this.state.filter.trim();
 
-clickLoadMore = (e) =>  {
-  e.preventDefault(); // Зупиняємо оновлення сторінки
-  console.log("start");
-  console.log(PAGE);
-  console.log(openList);
+
+  this.setState({imagelist: []}); // Чистемо сторінку
+  this.setState({totalHits: 0});// Скидаємо лічильник
+  this.setState({page: 2});// Оновлюємо номер сторінки
   
-  PAGE +=1;
-  let oldImagelist = this.state.imagelist.hits;
-  console.log(oldImagelist);
-  const newPage = fetch (`https://pixabay.com/api/?q=${openList}&page=${PAGE}&key=${API_CAY}&image_type=photo&orientation=horizontal`)
-  .then(res => res.json());
-  console.log(newPage);
+
+  if (this.state.filter.trim() === ''){
+    alert(`Filter window is empty`);
+    return;
+  } else {
+
+    this.setState({loading: true});// Зупecкаємо Лоадер
+    // отримаємо данні з сервера
+    fetch (`${BASE_URL}?q=${PHOTO_NAME}&page=1&key=${MY_API_KEY}&image_type=photo&orientation=horizontal&per_page=12`)
+    .then(res => res.json())
+    .then (imagelist => this.setState({imagelist}))
+    .finally(() => this.setState({loading: false}))
+    .catch(error => {error.alert(`Image not find`)})
+  
+    // this.setState({totalHits: this.state.imagelist.total});
+    // this.setState({stopPage: Math.round(this.state.imagelist.total /12)});
+    this.setState( { filter: ''}); // очищення вмісту форми
+    console.log(this.state.imagelist.total);
+    console.log(this.state.imagelist.hits.length);
+  }
+  };
+
+// LOAD MORE - дозавантиження 
+clickLoadMore = (e) =>  {
+
+
+  e.preventDefault(); // Зупиняємо оновлення сторінки
+  this.setState({page: this.state.page+1}); 
+
+  console.log(this.state.imagelist.total);
+  console.log(this.state.imagelist.hits.length);
+
+
+    if ( this.state.imagelist.total > this.state.imagelist.hits.length){
+      console.log("Add image");
+      this.setState({loading: true});// Зупecкаємо Лоадер
+      // отримаємо данні з сервера
+      fetch (`${BASE_URL}?q=${PHOTO_NAME}&page=1&key=${MY_API_KEY}&image_type=photo&orientation=horizontal&per_page=${this.state.page*12}`)
+      .then(res => res.json())
+      .then (imagelist => this.setState({imagelist}))
+      .finally(() => this.setState({loading: false}))
+    } 
+
 }
 
 // OPEN MODAL  - відкриття модалки
@@ -76,12 +105,26 @@ closeModal = () => {
   });
 };
 
+// ПЕРЕВІРКА для кнопки Load more (відображаємо чи ні)
+checkBtnLoad = () =>{
+  if (this.state.imagelist.hits) {
+    console.log(this.state.imagelist.total);
+    console.log(this.state.imagelist.hits.length);
+    if (this.state.imagelist.total > this.state.imagelist.hits.length ) {
+      showBtn = true;
+    } else { 
+      showBtn = false;
+    };
+  }
+
+}
 
 // РЕНДНЕРІНГ сторінки
   render(){
-    // const imagelist = this.state.imagelist.hits;
     const {filter, imagelist, loading, showModal, largeImageURL, alt } = this.state;
-    // let visible = false;
+
+    this.checkBtnLoad()
+
 
     return (
       <div>
@@ -90,8 +133,9 @@ closeModal = () => {
         </section>
         <section>
           <ImageGallery  imagelist={imagelist.hits} openModal={this.openModal}/>
-              { loading.hits && (<Loader/>) }
-              { imagelist.hits && (<Button clickLoadMore={this.clickLoadMore}/>) }
+              { loading && (<Loader/>) }
+
+              { showBtn && (<Button clickLoadMore={this.clickLoadMore}/>) }
         </section>
         {showModal && (
           <Modal closeModal={this.closeModal} src={largeImageURL} alt={alt}/>
